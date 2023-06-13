c4f04621cd6ea982e04c70e8f52e73b68780035e
ayichung <121764547+ayichung@users.noreply.github.com>
2023-06-13 13:58:07 -0400

    Write playRound function in game.js

--- game.js
+++ game.js
@@ -5,9 +5,23 @@ function getComputerChoice() {
 }
 
 function playRound(playerSelection, computerSelection) {
-
+    if (playerSelection == "rock") {
+        if (computerSelection == "rock") { return "tie" }
+        if (computerSelection == "paper") { return "computer" }
+        if (computerSelection == "scissors") { return "player" }
+    }
+    if (playerSelection == "paper") {
+        if (computerSelection == "rock") { return "player" }
+        if (computerSelection == "paper") { return "tie" }
+        if (computerSelection == "scissors") { return "computer" }
+    }
+    if (playerSelection == "scissors") {
+        if (computerSelection == "rock") { return "computer" }
+        if (computerSelection == "paper") { return "player" }
+        if (computerSelection == "scissors") { return "tie" }
+    }
 }
 
 function game() {
-
+    
 }
